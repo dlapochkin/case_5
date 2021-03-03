b9ea8cc770ee0d35858bc80939b74ec5a134dedd
ToliaKuznetsov <73088456+ToliaKuznetsov@users.noreply.github.com>
2021-03-03 10:04:52 +0700

    commit

--- main.py
+++ main.py
@@ -6,4 +6,10 @@ text = str(s)
 part_name = text.find("nfl-c-player-header__title")
 name = text[text.find('>',part_name)+1:text.find('</h1',part_name)]
 print(name)
+url = 'https://www.nfl.com/players/bryce-petty/stats/'
+f = urllib.request.urlopen(url)
+s = f.read()
+text = str(s)
+part_name = text.find("nfl-c-player-header__title")
+name = text[text.find('>',part_name)+1:text.find('</h1',part_name)]
 print(name)

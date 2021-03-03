d086560b694d5d4fcab2a4affc6530ccff8131f2
dkrivoshapova <d.krivoshapova@g.nsu.ru>
2021-03-03 10:19:34 +0700

    start

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-"""Case-study #4 Парсинг web-страниц
-Разработчики:
-Иванов А.С., Петров П.С., Сидоров C.H.
-""" 
+
 import urllib.request
-url = 'https://www.nfl.com/players/bryce-petty/stats/'
-f = urllib.request.urlopen(url)
-s = f.read()
-with open('html.txt', 'w') as out:
-  out.write(s)
+with open('input.txt') as f_in:
+    for line in f_in:
+        print(line)
+        f = urllib.request.urlopen(line)
+        s = f.read()
+        text = str(s)
+        part_name = text.find("nfl-c-player-header__title")
+        name = text[text.find('>',part_name)+1:text.find('</h1',part_name)]
+        with open('output.txt','w') as f_out:
+            print(name, file=f_out)
+            part_att = text.find("passingAttempts")
+            att = text[text.find('>',part_att)+1:text.find('</th>',part_att)]
+            u=''
+            for l in att:
+                if l.isdigit():
+                    u+=l
+                print(u, file=f_out)

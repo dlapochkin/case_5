592e6ab6f95159696bfacc04cc193fed33105a68
dkrivoshapova <d.krivoshapova@g.nsu.ru>
2021-03-03 16:09:08 +0700

    no pr

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ with open('output.txt','w') as f_out:
             text = str(s)
             part_name = text.find("nfl-c-player-header__title")
             name = text[text.find('>',part_name)+1:text.find('</h1',part_name)]
+            """part_rt = text.find('passingPasserRatung')
+            rt = text[text.find('>', part_rt) + 1:text.find('</h1', part_rt)]
+            rt = str(format(float(rt), '.2f'))"""
 
             part= text[text.find("passingAttempts")+1:text.find("passingFirstdowns")]
             n=[]
@@ -16,4 +19,4 @@ with open('output.txt','w') as f_out:
                 u=u.replace('n','')
                 n.append(u.strip())
                 part=part[part.find('</th>')+5:]
-            print('{:20s}{:<7s}{:<7s}{:<7s}{:<7s}{:<7s}'.format(name, n[1], n[0], n[3], n[6], n[7]),file=f_out)
+            print('{:20s}{:<7s}{:<7s}{:<7s}{:<7s}{:<7s}'.format(name, n[1], n[0], n[3], n[6], n[7]), file=f_out)

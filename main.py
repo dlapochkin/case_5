aa72f1400e55f1e8062f51ec9a3af40b0e2a7132
dlapochkin <73088970+dlapochkin@users.noreply.github.com>
2021-03-03 09:33:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-
+"""Case-study #4 Парсинг web-страниц
+Разработчики:
+Иванов А.С., Петров П.С., Сидоров C.H.
+""" 
+import urllib.request
+url = 'https://www.nfl.com/players/bryce-petty/stats/'
+f = urllib.request.urlopen(url)
+s = f.read()
+with open('html.txt', 'w') as out:
+  out.write(s)
